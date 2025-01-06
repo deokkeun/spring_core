c4a6d9424db4b7a7c6413e20ca98c03979a05d02
김덕근 <deokk91@gmail.com>
2025-01-07 00:04:31 +0900

    findBean

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,6 +15,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // 스프링은 빈을 생성하고, 의존관계를 주입하는 단계가 나누어져 있다
+    // 동적인 객체 인스턴스 의존관계 주입
+
+    // 빈 이름은 항상 다른 이름을 부여
     @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
